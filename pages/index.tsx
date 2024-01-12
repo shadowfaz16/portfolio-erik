5d794561162176b83ac5d6a13df93df009ab45a6
shadowfaz16 <erikfazju@gmail.com>
2024-01-12 16:46:04 -0300

    clients

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ type Props = {
 }
 
 
-const Home = ({pageInfo, experiences, clients, projects, socials}: Props) => {
+const Home = ({ pageInfo, clients, projects, socials }: Props) => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-purple-500/50 ">
       <Head>
@@ -55,7 +55,7 @@ const Home = ({pageInfo, experiences, clients, projects, socials}: Props) => {
 
       {/* Experience */}
       <section id="experience" className="snap-center">
-        <WorkExperience experiences={experiences} />
+        <WorkExperience />
       </section>
 
       {/* Clients */}
